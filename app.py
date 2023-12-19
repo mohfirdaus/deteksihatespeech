31d4c385109cf2b2221678457eaaa6745308bad7
Moh Firdaus <mohammedfirdaus1404@gmail.com>
2023-12-19 16:34:35 +0700

    update req txt

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # Menentukan jalur ke file requirements.txt
 requirements_file = os.path.join(current_directory, "requirements.txt")
 
-# Mengeksekusi perintah pip install -r requirements.txt
 try:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_file])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "-r", requirements_file])
 except subprocess.CalledProcessError as e:
     print(f"Error during installation: {e}")
     sys.exit(1)
